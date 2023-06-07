368842a8c153a56065a56008acc91bbc8285e36e
avkotau <kotov.alexander1@mail.ru>
2023-06-07 12:49:55 +0200

    feat: add select navigation with mouse

--- src/components/CustomSelector/CustomSelector.tsx
+++ src/components/CustomSelector/CustomSelector.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { KeyboardEvent, useEffect, useState } from "react";
 import st from './CustomSelector.module.css'
 
 
@@ -22,14 +22,37 @@ export const CustomSelector: React.FC<SelectPropsType> = ({
 
     const [active, setActive] = useState(false);
 
-    const [] = useState();
+    const [hoveredElement, setHoveredElement] = useState(value);
+    console.log('hoveredElement',hoveredElement)
+    const changedItem = items.find(cp => cp.value === value)
+    const hoveredItem = items.find(cp => cp.value === hoveredElement)
+
+    useEffect(() => {
+        setHoveredElement(value)
+    },[value])
+
+    const onKeyUpHandler = (e: KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === 'ArrowDown' || e.key === 'ArrowUp'){
+
+    }
+        for (let i = 0; i < items.length; i++) {
+            if (items[i].value === hoveredElement){
+                const pretendentElement = e.key === 'ArrowDown'
+                ? items[i+1].value
+                    : items[i-1].value
+                if (pretendentElement) {
+                    console.log('items[i+1]',  items[i+1].value)
+                    onchange(items[i+1].value)
+                }
+
+            }
+        }
+    }
 
 
     const onchangeValueHandler = () => {
         setActive(!active)
     }
-
-    const changedItem = items.find(cp => cp.value === value)
 
     const callMultipleFunctions = (value: string) => {
         onchange(value)
@@ -41,12 +64,17 @@ export const CustomSelector: React.FC<SelectPropsType> = ({
             <div className={st.selector}>
 
                 <span onClick={onchangeValueHandler}>{changedItem && changedItem.capital}</span>
-                <div className={active ? st.show : st.toggle}>
+                <div className={active ? st.show : st.toggle}
+                    onKeyUp={onKeyUpHandler}
+                     tabIndex={0}
+                >
                     {items.map((ct, i) => (
 
                         <div key={ct.value}
+                             tabIndex={i+1}
+                             onMouseEnter={() => setHoveredElement(ct.value)}
                              onClick={() => callMultipleFunctions(ct.value)}
-                             className={st.items}
+                             className={hoveredItem === items[i] ? st.items + ' ' + st.itemSelected : st.items}
                         >{ct.capital}</div>
 
                     ))}
